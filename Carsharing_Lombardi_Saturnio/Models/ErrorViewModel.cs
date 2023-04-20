fcadb598d005d9fe78dac64a88039f415e5cc310
MAXIME <MAXIME@LAPTOP-4H37GGAT>
2023-04-20 10:43:59 +0200

    Add project files.

---
+++ Carsharing_Lombardi_Saturnio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Carsharing_Lombardi_Saturnio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
